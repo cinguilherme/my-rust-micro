96436f34c68ccaaf9ef50979ca53b7c4bee6f068
cinguilherme <cinguilherme@gmail.com>
2020-06-02 10:29:49 -0300

    init discovery of how to handle request

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::convert::Infallible;
 use std::net::SocketAddr;
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Method, StatusCode};
 
 use tokio;
 
