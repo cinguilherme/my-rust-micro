0ebf266a389c1375e66dfcc444c895bc0ec0a932
cinguilherme <cinguilherme@gmail.com>
2020-06-02 09:52:45 -0300

    adding use statements to collect all required deps for the service to run

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::fmt;
+use std::sync::{Arc, Mutex};
 use slab::Slab;
-use hyper::Server;
-
-
+use regex::Regex;
+use futures::{future, Future};
+use hyper::service::service_fn;
+use hyper::{Server, Body, Request, Response, Error, Method};
 
 fn main() {
     println!("Hello, world!");
