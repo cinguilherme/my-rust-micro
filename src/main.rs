0fd139162af4da195d1f31478e4cb4720d843de3
cinguilherme <cinguilherme@gmail.com>
2020-06-02 10:23:33 -0300

    hello world server running

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-use std::fmt;
-use std::sync::{Arc, Mutex};
-use slab::Slab;
-use regex::Regex;
-use futures::{future, Future};
-use hyper::service::service_fn;
-use hyper::{Server, Body, Request, Response, Error, Method};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
 
-fn main() {
-    println!("Hello, world!");
+use tokio;
+
+async fn handle(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello, World!".into()))
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(handle))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
